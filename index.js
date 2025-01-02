ea41f17a8d3e67191b43f3cea78bae2352555599
mahirabidi12 <abidimahir109@gmail.com>
2025-01-02 22:36:24 +0530

    Initial Commit For Pokemon Project

---
+++ index.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("interface");
+const displayError = document.getElementById("displayError");
+
+form.addEventListener("submit" ,async (event) => {
+    event.preventDefault();
+    displayError.textContent="";
+    const pokemonName = document.getElementById("pokemonName").value;
+    
+    if(pokemonName){
+        try{
+            const data = await getData(pokemonName);
+            displayData(data);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    else{
+        displayError.textContent = "Please enter a pokemon name"
+    }
+});
+
+
+
+async function getData(pokemonName) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if(!response.ok){
+        throw new Error("Could not get the rigth response from API");
+    }
+    return await response.json();
+}
+
+function displayData(data){
+    const displayPokemon = document.getElementById("displayPokemon");
+    displayPokemon.src = data.sprites.front_default
+    console.log(data);
+}
